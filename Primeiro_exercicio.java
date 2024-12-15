2abdbf523be157159dcfe5d3ba3952fe8af23742
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2024-12-15 12:04:19 -0300

    Primeiro_exercicio.java

---
+++ Primeiro_exercicio.java
@@ -0,0 +1,37 @@
+public class Primeiro_exercicio {
+
+    public static void main(String[] args){
+
+        System.out.println("Eu fiz este componente!");
+        System.out.println("Eu amo pizza");
+
+        //Caso utilizarmos o System.out.println() ele vai deixar para que pule para próxima linha, o ln vem de line.
+
+        //Se utilizarmos o "\t" no inicio do system.out.println ou print ele vai dar um tab no seu texto
+
+        System.out.println("\t Veja só!!");
+
+        //Caso você queira colocar aspas duplas no texto,
+
+        System.out.println("\" Eu amo pizza \"");
+
+        // Caso você queira exibir no console a "\"
+
+        System.out.println("\\ Estou exibindo um caractere especial! \\");
+
+        /*Isto
+         *é um
+         *commentario com mais
+         *de
+         * uma linha
+         */
+
+        //Atalho para este commando
+        System.out.println();
+        
+        //sysout + tab
+    }
+
+
+
+}
