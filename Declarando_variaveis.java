d18527a3afa9c154c9891afc18a5fb66373dbf8e
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2024-12-15 19:27:07 -0300

    Declarando_variaveis.java

---
+++ Declarando_variaveis.java
@@ -0,0 +1,12 @@
+public class Declarando_variaveis {
+
+    public static void main(String[] args){
+
+        int x = 2;
+        double y = 4.36;
+        boolean z = true;
+        String name = "Nicholas";
+
+        System.out.println("Hello " + name);
+    }
+}
