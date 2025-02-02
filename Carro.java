dc00e543de1568b757e9b22d771cba4bcf8bb802
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2025-02-02 14:40:54 -0300

    Carro.java

---
+++ Carro.java
@@ -0,0 +1,13 @@
+public class Carro{
+
+    String modelo;
+    String ano_de_lancamento;
+    double preco;
+
+    public Carro(String modelo, String ano_de_lancamento, double preco){
+
+        this.modelo = modelo;
+        this.ano_de_lancamento = ano_de_lancamento;
+        this.preco = preco;
+    };
+}
