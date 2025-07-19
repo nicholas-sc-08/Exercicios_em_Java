3875314e074223d3fea051b40c722fcc6951cbd0
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2025-07-18 21:13:27 -0300

    clientela.java

---
+++ clientela.java
@@ -0,0 +1,13 @@
+public class clientela {
+    
+    String nome;
+    String email;
+    int idade;
+
+    public clientela(String nome, int idade, String email){
+
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    };
+}
